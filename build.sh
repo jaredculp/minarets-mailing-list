162746bae1cda8b6b3a9f80579c821717c7c5bac
jaredculp <jculp14@gmail.com>
2020-04-17 10:04:16 -0400

    tidy up build script

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 rm page-*.html 2>/dev/null
-awk -f parse.awk < minarets-mailbox.txt > output
-csplit -s -f 'page-' output '/DOCTYPE/' '{99999}'
+awk -f parse.awk < minarets-mailbox.txt | csplit -s -f 'page-' - '/DOCTYPE/' '{99999}'
 for f in page-*
 do
   mv "$f" `echo "$f" | awk -F- '{printf("%s-%02d.html\n", $1, $2+1)}'`
 done
-rm output
